0db9b45c08c2b8ed27b6a8527a0ae2de1f45a461
Ala Eddine LUSAKULA <aladdinlusa@gmail.com>
2023-03-01 21:06:30 +0100

    changed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [password, setPassword] = useState("");
+
+  const urlParams = new URLSearchParams(window.location.search);
+  const userId = urlParams.get("userId");
+  const token = urlParams.get("token");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // Send a request to the server to reset the password
+    const response = await fetch(
+      `https://fierce-tick-overalls.cyclic.app/resetPassword?userId=${userId}&token=${token}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          password: password,
+        }),
+      }
+    );
+
+    // Handle the response from the server
+    const data = await response.json();
+    console.log(data);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Set New Password</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="password"
+          placeholder="New Password...."
+          value={password}
+          onChange={handlePasswordChange}
+        />
+        <br />
+        <br />
+        <button type="submit">Confirm</button>
+      </form>
     </div>
   );
 }
